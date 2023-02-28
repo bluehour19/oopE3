bbbe07222d3454882a2e10328bc791273e5ae695
bluehour19 <105144782+bluehour19@users.noreply.github.com>
2023-02-28 17:35:03 +0200

    Update FileNamelab2ex2.cpp

--- FileNamelab2ex2.cpp
+++ FileNamelab2ex2.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 #include <cstring>
 #include "student.h"
-
+int compareMgrade(student Student1, student Student2)
+{
+	if (Student1.getMgrade() > Student2.getMgrade())
+	{
+		return 1;
+	}
+	else return -1;
+	if (Student1.getMgrade() == Student2.getMgrade())
+		return 0;
+}
+int compareEgrade(student Student1, student Student2)
+{
+	if (Student1.getEgrade() > Student2.getEgrade())
+	{
+		return 1;
+	}
+	else return -1;
+	if (Student1.getEgrade() == Student2.getEgrade())
+		return 0;
+}
+int compareHgrade(student Student1, student Student2)
+{
+	if (Student1.getHgrade() > Student2.getHgrade())
+	{
+		return 1;
+	}
+	else return -1;
+	if (Student1.getHgrade() == Student2.getHgrade())
+		return 0;
+}
+int compareAvggrade(student Student1, student Student2)
+{
+	if (Student1.getAvgGrade() > Student2.getAvgGrade())
+	{
+		return 1;
+	}
+	else return -1;
+	if (Student1.getHgrade() == Student2.getHgrade())
+		return 0;
+}
+int compareName(student Student1, student Student2)
+{
+	if (strcmp(Student1.getName(), Student2.getName()) > 0) return 1;
+	else return -1;
+	if (strcmp(Student1.getName(), Student2.getName()) == 0) return 0;
+}
 int main()
 {
 	char p[100],  q[100];
@@ -17,5 +62,10 @@ int main()
 	Student2.setHgrade(8);
 	Student1.setName(p);
 	Student2.setName(q);
+        printf("%d\n", compareMgrade(Student1, Student2));
+	printf("%d\n", compareEgrade(Student1, Student2));
+	printf("%d\n", compareHgrade(Student1, Student2));
+	printf("%d\n", compareAvggrade(Student1, Student2));
+	printf("%d\n", compareName(Student1, Student2));
 	return 0;
 }
