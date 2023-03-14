01bfa8cd41da546de561516b2e2726e133932eff
bluehour19 <105144782+bluehour19@users.noreply.github.com>
2023-03-14 17:38:06 +0200

    Update Sort.h

--- lab.4/Sort.h
+++ lab.4/Sort.h
@@ -1,15 +1,16 @@
 class Sort
 {
+private: 
     // add data members
     int *vector;
     int Nr_elements;
-    int max_value;
-    int min_value;
+    int  Partition(int low, int high, bool ascendent);
+    void quick(int low, int high, bool ascendent);
 public:
 
     Sort(int number_of_elements, int max_element, int min_element);
     Sort();
-    Sort(int* vector, int number_of_elements);
+    Sort(int array[], int number_of_elements);
     Sort(int count, ...);
     Sort(char* c);
     // add constuctors
