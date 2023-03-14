1797c4b9a405a3266c393a138bd38ddf06b06668
bluehour19 <105144782+bluehour19@users.noreply.github.com>
2023-03-14 17:38:30 +0200

    Update main.cpp

--- lab.4/main.cpp
+++ lab.4/main.cpp
@@ -1,15 +1,23 @@
-
 #include "Sort.h"
 #include <stdlib.h>
 int main()
 {
-	Sort c1(10, 10, 100);
+	//create a list with randomised values
+	Sort s1(10, 100, 10);
+	s1.Print();
+	//create a list with existing values
 	int v[5] = { 1, 2, 3, 4, 5 };
-	Sort c2(v, 5);
-	Sort c3(6, 1, 2, 3, 4, 5, 6);
-	char c[] = "12,34,56,78";
-	Sort c4(c);
-	Sort c5();
+	Sort s3(v, 5);
+	s3.Print();
+	//create a list with a variatic number of values 
+	Sort s4(6, 1, 2, 3, 4, 5, 6);
+	s4.Print();
+	//create a list from a char *
+	char c[] = "34,12,56,78";
+	Sort s5(c);
+	s5.Print();
+	s5.QuickSort(true);
+	s5.Print();
 	system("pause");
 	return 0;
 }
